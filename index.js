edce138489bee5c45158e9f31cbfd85eb6346c86
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-08-06 00:01:53 +0300

    lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({ name, email })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newId = document.createElement('p');
+        newId.innerText = `ID: ${data.id}`;
+        document.body.appendChild(newId);
+    })
+    .catch(error => {
+        const errorMessage = document.createElement('p');
+        errorMessage.innerText = `Error: ${error.message}`;
+        document.body.appendChild(errorMessage);
+    });
+}
+
+
+
+
